8fa613032c50af3bd5ed04f5bb7d23503b4d7c20
Jacob Bashista <tgb.bashista@gmail.com>
2022-10-07 12:23:27 -0400

    Now with GUI

--- main.py
+++ main.py
@@ -1,107 +1,285 @@
+from time import time
 import cv2
 import numpy as np
 import serial
-from time import time
 
-cam = cv2.VideoCapture(0)
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
 
-first_frame = None
 
-ser = serial.Serial(
-    port='/dev/cu.usbmodem1424401',
-    baudrate = 9600,
-    timeout=1
-)
+class Window(QtWidgets.QMainWindow):
+    def setupUi(self, MainWindow):
 
-# reset the timers
-ser.write(0)
+        self.status = 'reset'
+        self.startTime = 0
+        self.leftTime = 0
+        self.rightTime = 0
 
-status = 'reset'
-race = True
-start_time = 0
-left_time = 0
-right_time = 0
+        MainWindow.setObjectName('BrickTimer')
+        MainWindow.resize(498, 522)
 
-while race:
-    line = ser.readline() 
-    if line:
-        status = line.decode().strip()
+        # Primary Layout
+        layout = QtWidgets.QVBoxLayout()
 
-        while status == 'go':
-            if start_time == 0:
-                start_time = int(time() * 1000)
+        # Top most bar
+        barLayout = QtWidgets.QHBoxLayout()
 
-            check, frame = cam.read()
+        # Enter top bar
+        self.startButton = QtWidgets.QPushButton('Start')
+        self.startButton.clicked.connect(self.showCamera)
+        barLayout.addWidget(self.startButton)
+
+        self.enableSerial = QtWidgets.QCheckBox()
+        self.enableSerial.setText('Enable Serial')
+        barLayout.addWidget(self.enableSerial)
+
+        topBar = QtWidgets.QWidget()
+        topBar.setLayout(barLayout)
+
+        layout.addWidget(topBar)
+        # Leave top bar
+
+        # Enter slider area
+        slidersLayout = QtWidgets.QHBoxLayout()
+
+        # Enter left controls
+        leftControlsLayout = QtWidgets.QVBoxLayout()
+
+        leftLabel = QtWidgets.QLabel('Left Controls')
+        leftControlsLayout.addWidget(leftLabel)
+
+        # Left Width Slider
+        leftWidthLabel = QtWidgets.QLabel('Left Width')
+        leftControlsLayout.addWidget(leftWidthLabel)
+        self.leftWidth = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.leftWidth.setValue(50)
+        leftControlsLayout.addWidget(self.leftWidth)
+
+        # Left Height Slider
+        leftHeightLabel = QtWidgets.QLabel('Left Height')
+        leftControlsLayout.addWidget(leftHeightLabel)
+        self.leftHeight = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.leftHeight.setValue(50)
+        leftControlsLayout.addWidget(self.leftHeight)
+
+        # Left X Slider
+        leftXLabel = QtWidgets.QLabel('Left X')
+        leftControlsLayout.addWidget(leftXLabel)
+        self.leftX = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.leftX.setValue(50)
+        leftControlsLayout.addWidget(self.leftX)
+
+        # Left Y Slider
+        leftYLabel = QtWidgets.QLabel('Left Y')
+        leftControlsLayout.addWidget(leftYLabel)
+        self.leftY = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.leftY.setValue(50)
+        leftControlsLayout.addWidget(self.leftY)
+
+        leftControls = QtWidgets.QWidget()
+        leftControls.setLayout(leftControlsLayout)
+        slidersLayout.addWidget(leftControls)
+        # Leave left controls
+
+        # Enter right controls
+        rightControlsLayout = QtWidgets.QVBoxLayout()
+
+        rightLabel = QtWidgets.QLabel('Right Controls')
+        rightControlsLayout.addWidget(rightLabel)
+
+        # Right Width Slider
+        rightWidthLabel = QtWidgets.QLabel('Right Width')
+        rightControlsLayout.addWidget(rightWidthLabel)
+        self.rightWidth = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.rightWidth.setValue(50)
+        rightControlsLayout.addWidget(self.rightWidth)
+
+        # Right Height Slider
+        rightHeightLabel = QtWidgets.QLabel('Right Height')
+        rightControlsLayout.addWidget(rightHeightLabel)
+        self.rightHeight = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.rightHeight.setValue(50)
+        rightControlsLayout.addWidget(self.rightHeight)
+
+        # Right X Slider
+        rightXLabel = QtWidgets.QLabel('Right X')
+        rightControlsLayout.addWidget(rightXLabel)
+        self.rightX = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.rightX.setValue(50)
+        rightControlsLayout.addWidget(self.rightX)
+
+        # Right Y Slider
+        rightYLabel = QtWidgets.QLabel('Right Y')
+        rightControlsLayout.addWidget(rightYLabel)
+        self.rightY = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.rightY.setValue(50)
+        rightControlsLayout.addWidget(self.rightY)
+
+        rightControls = QtWidgets.QWidget()
+        rightControls.setLayout(rightControlsLayout)
+        slidersLayout.addWidget(rightControls)
+        # Leave right controls
+
+        sliderArea = QtWidgets.QWidget()
+        sliderArea.setLayout(slidersLayout)
+        layout.addWidget(sliderArea)
+        # Leave slider area
+
+        # Threshold controls
+        thresholdLayout = QtWidgets.QHBoxLayout()
+
+        thresholdLabel = QtWidgets.QLabel('Threshold')
+        thresholdLayout.addWidget(thresholdLabel)
+
+        minLabel = QtWidgets.QLabel('Min')
+        thresholdLayout.addWidget(minLabel)
+        self.min = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.min.setRange(0, 255)
+        self.min.setValue(75)
+        thresholdLayout.addWidget(self.min)
+
+        maxLabel = QtWidgets.QLabel('Max')
+        thresholdLayout.addWidget(maxLabel)
+        self.max = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.max.setRange(0, 255)
+        self.max.setValue(255)
+        thresholdLayout.addWidget(self.max)
+
+        self.showThresh = QtWidgets.QCheckBox()
+        self.showThresh.setText('Show Thresh')
+        thresholdLayout.addWidget(self.showThresh)
+
+        thresholdControls = QtWidgets.QWidget()
+        thresholdControls.setLayout(thresholdLayout)
+        layout.addWidget(thresholdControls)
+        # Leave threshold controls
+
+        # Primary Layout (renders video)
+        self.label = QtWidgets.QLabel()
+        layout.addWidget(self.label)
+
+        widget = QtWidgets.QWidget()
+        widget.setLayout(layout)
+        MainWindow.setCentralWidget(widget)
+
+    def showCamera(self):
+
+        self.status = 'reset'
+        self.startTime = 0
+        self.leftTime = 0
+        self.rightTime = 0
+
+        ser = None
+
+        if(self.enableSerial.checkState() == 2):
+            ser = serial.Serial(
+                port='/dev/cu.usbmodem1424401',
+                baudrate=9600,
+                timeout=1
+            )
+
+        if(ser):
+            ser.write(0)
+
+        self.startButton.setText('Reset')
+        cam = cv2.VideoCapture(1)
+
+        check, frame = cam.read()
+        frameHeight, frameWidth, _ = frame.shape
+        self.leftWidth.setRange(0, frameWidth)
+        self.leftHeight.setRange(0, frameHeight)
+        self.leftX.setRange(0, frameWidth)
+        self.leftY.setRange(0, frameHeight)
+
+        self.rightWidth.setRange(0, frameWidth)
+        self.rightHeight.setRange(0, frameHeight)
+        self.rightX.setRange(0, frameWidth)
+        self.rightY.setRange(0, frameHeight)
+
+        self.firstFrame = None
+        while True:
+            QtWidgets.QApplication.processEvents()
+
+            line = None
+            if(ser):
+                line = ser.readline()
+
+            if(line):
+                self.status = line.decode.strip()
+
+            if(self.status == 'go' and self.startTime == 0):
+                self.startTime = int(time() * 1000)
+
+            _, frame = cam.read()
 
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-            if first_frame is None:
-                first_frame = gray
+            if self.firstFrame is None:
+                self.firstFrame = gray
                 continue
 
-            frame_delta = cv2.absdiff(first_frame, gray)
-            thresh = cv2.threshold(frame_delta, 75, 255, cv2.THRESH_BINARY)[1]
+            frameDelta = cv2.absdiff(self.firstFrame, gray)
+            thresh = cv2.threshold(frameDelta,  self.min.value(
+            ), self.max.value(), cv2.THRESH_BINARY)[1]
             thresh = cv2.dilate(thresh, None, iterations=4)
 
-            # Crop needs to be adjusted to fit finish line
-            shape = thresh.shape
-            height = shape[0]
-            width = shape[1]
-            half_width = int(width/2)
-            quarter_width = int(half_width/2)
-            framed_height = int(height/3)
-            left_height = int(height - framed_height)
+            leftImage = thresh[self.leftY.value():self.leftY.value(
+            ) + self.leftHeight.value(), self.leftX.value():self.leftX.value() + self.leftWidth.value()]
 
-            left = frame[left_height:height, quarter_width:half_width]
-            right = frame[left_height:height, half_width:width-quarter_width]
+            rightImage = thresh[self.rightY.value():self.rightY.value(
+            ) + self.rightHeight.value(), self.rightX.value():self.rightX.value() + self.rightWidth.value()]
 
-            left_thresh = thresh[0:height, 0:half_width]
-            right_thresh = thresh[0:height, half_width:width]
+            contoursLeft, _ = cv2.findContours(
+                image=leftImage, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 
-            contours_left, _ = cv2.findContours(image=left_thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
-            cv2.drawContours(image=left, contours=contours_left, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+            contoursRight, _ = cv2.findContours(
+                image=rightImage, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 
-            contours_right, _ = cv2.findContours(image=right_thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
-            cv2.drawContours(image=right, contours=contours_right, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+            if(len(contoursLeft) >= 1 and self.leftTime == 0):
+                # Send left track timer stop
+                if(ser):
+                    ser.write(bytes('1', 'utf-8'))
+                self.leftTime = int(time() * 1000) - self.startTime
 
-            if(len(contours_left) >= 1 and left_time == 0):
-                # Send left track timer stop 
-                ser.write(bytes('1', 'utf-8'))
-                left_time = int(time() * 1000) - start_time
-                # write to database
-     
-            if(len(contours_right) >= 1 and right_time == 0):
+            if(len(contoursRight) >= 1 and self.rightTime == 0):
                 # Send right track timer stop
-                ser.write(bytes('2', 'utf-8'))
-                right_time = int(time() * 1000) - start_time
-                # write to database
+                if(ser):
+                    ser.write(bytes('2', 'utf-8'))
+                self.rightTime = int(time() * 1000) - self.startTime
 
-            if left_time != 0:
-                cv2.putText(left, str(left_time), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA) 
+            # Draw times
+            if(self.leftTime != 0):
+                cv2.putText(frame, str(self.leftTime), (self.leftX.value() + 10, self.leftY.value() + 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            if(self.rightTime != 0):
+                cv2.putText(frame, str(self.rightTime), (self.rightX.value() + 10, self.rightY.value() + 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-            if right_time != 0:
-                cv2.putText(right, str(right_time), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+            # Draw rectangles
+            leftRectangle = cv2.rectangle(
+                frame, (self.leftX.value(), self.leftY.value()), (self.leftX.value() + self.leftWidth.value(), self.leftY.value() + self.leftHeight.value()), (0, 0, 255), 2)
 
-            cv2.imshow('left', left)
-            cv2.imshow('right', right)
+            rightRectangle = cv2.rectangle(
+                leftRectangle, (self.rightX.value(), self.rightY.value()), (self.rightX.value() + self.rightWidth.value(), self.rightY.value() + self.rightHeight.value()), (0, 255, 0), 2)
 
-            # cv2.imshow('thresh', thresh)
+            rgbFrame = None
 
-            key = cv2.waitKey(1)
-            if key == 27:
-                # ESC to quit
-                race = False
-                break 
-            elif key == 32:
-                # SPACE to reset
-                status = 'reset'
-                start_time = 0
-                left_time = 0
-                right_time = 0
-                first_frame = gray
-                ser.write(bytes('0', 'utf-8'))
+            if(self.showThresh.checkState() == 2):
+                rgbFrame = cv2.cvtColor(thresh, cv2.COLOR_GRAY2RGB)
+            else:
+                rgbFrame = cv2.cvtColor(rightRectangle, cv2.COLOR_BGR2RGB)
 
-cam.release()
-cv2.destroyAllWindows()
-ser.close()
+            qtImage = QtGui.QImage(
+                rgbFrame, rgbFrame.shape[1], rgbFrame.shape[0], rgbFrame.strides[0], QtGui.QImage.Format_RGB888)
+            self.label.setPixmap(QtGui.QPixmap.fromImage(qtImage))
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    MainWindow.setWindowTitle('BrickTimer')
+    ui = Window()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
